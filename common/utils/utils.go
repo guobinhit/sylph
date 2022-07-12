24cd760c1d364853c1fdff84b022e45d965ed6e5
guobin.1120 <guobin.1120@bytedance.com>
2022-07-12 10:20:01 +0800

    feat: add Json and DeepCopy method

--- common/utils/utils.go
+++ common/utils/utils.go
@@ -1,9 +1,29 @@
 package utils
 
+import (
+	"encoding/json"
+	"github.com/guobinhit/sylph/constant/string_const"
+)
+
 // If functions similar to ternary operators，if b is ture return f, else return s
 func If(b bool, f, s interface{}) interface{} {
 	if b {
 		return f
 	}
 	return s
+}
+
+// Json returns a json string.
+func Json(i interface{}) string {
+	if i == nil {
+		return string_const.EmptyString
+	}
+	v, _ := json.Marshal(i)
+	return string(v)
+}
+
+// DeepCopy source and target must have the same structure, and target must be a pointer.
+func DeepCopy(source, target interface{}) {
+	bytes, _ := json.Marshal(source)
+	_ = json.Unmarshal(bytes, target)
 }
--- common/utils/utils_test.go
+++ common/utils/utils_test.go
@@ -10,3 +10,39 @@ func TestIf(t *testing.T) {
 	is.Equal(1, If(true, 1, 2))
 	is.Equal(2, If(false, 1, 2))
 }
+
+func TestJson(t *testing.T) {
+	is := assert.New(t)
+	is.Equal("1", Json(1))
+	is.Equal(`"a"`, Json("a"))
+	is.Equal("true", Json(true))
+	is.Equal("", Json(nil))
+	is.Equal(`{"key":"age","value":18}`, Json(struct {
+		Key   string `json:"key"`
+		Value int    `json:"value"`
+	}{Key: "age", Value: 18}))
+}
+
+func TestDeepCopy(t *testing.T) {
+	is := assert.New(t)
+	sourceMap := map[string]int{
+		"a": 1,
+		"b": 2,
+		"c": 3,
+	}
+	targetMap := &map[string]int{}
+	DeepCopy(sourceMap, targetMap)
+	is.Equal(Json(targetMap), Json(sourceMap))
+
+	type testStruct struct {
+		Key   string `json:"key"`
+		Value int    `json:"value"`
+	}
+	source := &testStruct{
+		Key:   "abc",
+		Value: 123,
+	}
+	target := &testStruct{}
+	DeepCopy(source, target)
+	is.Equal(Json(source), Json(target))
+}
