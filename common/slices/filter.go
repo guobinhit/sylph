a9062e900f74eb7e870b5d3d8df814fa6bbf55b4
guobin.1120 <guobin.1120@bytedance.com>
2022-07-17 15:38:35 +0800

    feat: enhance slices util

--- common/slices/filter.go
+++ common/slices/filter.go
@@ -12,9 +12,69 @@ func FilterInt(s []int, fn func(v int) bool) []int {
 	return ans
 }
 
+// FilterInt8 applies a fn to each element of s, return a slices of make fn true.
+func FilterInt8(s []int8, fn func(v int8) bool) []int8 {
+	ans := make([]int8, 0)
+	for _, v := range s {
+		isOk := fn(v)
+		if isOk {
+			ans = append(ans, v)
+		}
+	}
+	return ans
+}
+
+// FilterInt16 applies a fn to each element of s, return a slices of make fn true.
+func FilterInt16(s []int16, fn func(v int16) bool) []int16 {
+	ans := make([]int16, 0)
+	for _, v := range s {
+		isOk := fn(v)
+		if isOk {
+			ans = append(ans, v)
+		}
+	}
+	return ans
+}
+
+// FilterInt32 applies a fn to each element of s, return a slices of make fn true.
+func FilterInt32(s []int32, fn func(v int32) bool) []int32 {
+	ans := make([]int32, 0)
+	for _, v := range s {
+		isOk := fn(v)
+		if isOk {
+			ans = append(ans, v)
+		}
+	}
+	return ans
+}
+
 // FilterInt64 applies a fn to each element of s, return a slices of make fn true.
 func FilterInt64(s []int64, fn func(v int64) bool) []int64 {
 	ans := make([]int64, 0)
+	for _, v := range s {
+		isOk := fn(v)
+		if isOk {
+			ans = append(ans, v)
+		}
+	}
+	return ans
+}
+
+// FilterFloat32 applies a fn to each element of s, return a slices of make fn true.
+func FilterFloat32(s []float32, fn func(v float32) bool) []float32 {
+	ans := make([]float32, 0)
+	for _, v := range s {
+		isOk := fn(v)
+		if isOk {
+			ans = append(ans, v)
+		}
+	}
+	return ans
+}
+
+// FilterFloat64 applies a fn to each element of s, return a slices of make fn true.
+func FilterFloat64(s []float64, fn func(v float64) bool) []float64 {
+	ans := make([]float64, 0)
 	for _, v := range s {
 		isOk := fn(v)
 		if isOk {
--- common/slices/filter_test.go
+++ common/slices/filter_test.go
@@ -15,6 +15,36 @@ func TestFilterInt(t *testing.T) {
 	}))
 }
 
+func TestFilterInt8(t *testing.T) {
+	is := assert.New(t)
+	is.Equal([]int8{11, 20}, FilterInt8([]int8{11, 20}, func(v int8) bool {
+		return v > 0
+	}))
+	is.Equal([]int8{20}, FilterInt8([]int8{11, 20}, func(v int8) bool {
+		return v%2 == 0
+	}))
+}
+
+func TestFilterInt16(t *testing.T) {
+	is := assert.New(t)
+	is.Equal([]int16{11, 20}, FilterInt16([]int16{11, 20}, func(v int16) bool {
+		return v > 0
+	}))
+	is.Equal([]int16{20}, FilterInt16([]int16{11, 20}, func(v int16) bool {
+		return v%2 == 0
+	}))
+}
+
+func TestFilterInt32(t *testing.T) {
+	is := assert.New(t)
+	is.Equal([]int32{11, 20}, FilterInt32([]int32{11, 20}, func(v int32) bool {
+		return v > 0
+	}))
+	is.Equal([]int32{20}, FilterInt32([]int32{11, 20}, func(v int32) bool {
+		return v%2 == 0
+	}))
+}
+
 func TestFilterInt64(t *testing.T) {
 	is := assert.New(t)
 	is.Equal([]int64{11, 20}, FilterInt64([]int64{11, 20}, func(v int64) bool {
@@ -22,6 +52,26 @@ func TestFilterInt64(t *testing.T) {
 	}))
 	is.Equal([]int64{20}, FilterInt64([]int64{11, 20}, func(v int64) bool {
 		return v%2 == 0
+	}))
+}
+
+func TestFilterFloat32(t *testing.T) {
+	is := assert.New(t)
+	is.Equal([]float32{11, 20}, FilterFloat32([]float32{11, 20}, func(v float32) bool {
+		return v > 0
+	}))
+	is.Equal([]float32{20}, FilterFloat32([]float32{11, 20}, func(v float32) bool {
+		return v > 15
+	}))
+}
+
+func TestFilterFloat64(t *testing.T) {
+	is := assert.New(t)
+	is.Equal([]float64{11, 20}, FilterFloat64([]float64{11, 20}, func(v float64) bool {
+		return v > 0
+	}))
+	is.Equal([]float64{20}, FilterFloat64([]float64{11, 20}, func(v float64) bool {
+		return v > 15
 	}))
 }
 
